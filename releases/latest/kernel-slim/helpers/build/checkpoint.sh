aea3dd80650e914e461ad2ec2794740d6ff09da9
Younes Manton <ymanton@ca.ibm.com>
2021-04-15 17:27:31 -0700

    CRIU support

---
+++ releases/latest/kernel-slim/helpers/build/checkpoint.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+#if [ "$VERBOSE" != "true" ]; then
+#  exec &>/dev/null
+#fi
+
+set -Eeox pipefail
+
+if [ $# -gt 0 ]
+then
+    image="$1"
+    containername="checkpoint.$$"
+    docker run --name="$containername" --privileged --user=root --tmpfs=/run -v /lib/modules:/lib/modules "$image" checkpoint.sh
+    docker commit --change='CMD ["criu", "restore", "-D", "/output/checkpoint", "--shell-job", "--tcp-established", "--file-locks"]' "$containername" "checkpoint:$$"
+    docker container rm "$containername"
+else
+    server start
+    serverPID="$( cat /opt/ol/wlp/output/.pid/defaultServer.pid )"
+    mkdir /output/checkpoint
+    criu dump -t "$serverPID" -D /output/checkpoint --shell-job --tcp-established --file-locks
+fi
